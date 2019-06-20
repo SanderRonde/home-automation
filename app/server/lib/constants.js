6d013141be043fe2120f8dc8ebf06a00aa152539
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-06-20 03:30:45 +0200

    ignore compiled js files

--- app/server/lib/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const ROOT = path.join(__dirname, '../../../');
-exports.DB_FOLDER = path.join(ROOT, 'database');
-exports.DB_FILE = path.join(exports.DB_FOLDER, 'db.json');
-exports.SECRETS_FOLDER = path.join(ROOT, 'secrets');
-exports.SECRETS_FILE = path.join(exports.SECRETS_FOLDER, 'secrets.txt');
--- app/server/lib/errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class KeyError extends Error {
-}
-exports.KeyError = KeyError;
-class AuthError extends Error {
-}
-exports.AuthError = AuthError;
