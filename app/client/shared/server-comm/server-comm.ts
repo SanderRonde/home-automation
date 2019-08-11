724f8888bc07fccd4aa722d52f88d2b062317a9d
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-08-11 20:39:15 +0200

    just hardcode the key

--- app/client/shared/server-comm/server-comm.ts
+++ app/client/shared/server-comm/server-comm.ts
@@ -2,6 +2,9 @@ import {
 	ConfigurableWebComponent, Props, PROP_TYPE, config, EventListenerObj
 } from "../../../../node_modules/wclib/build/es/wclib.js";
 import { MessageToast } from "../message-toast/message-toast.js";
+const AUTH = {
+	auth: btoa('***REMOVED***')
+};
 
 @config({
 	is: 'server-comm',
@@ -30,54 +33,6 @@ export abstract class ServerComm<ELS extends {
 		}
 	});
 
-	private _createClientSecret(id: number) {
-		const key = this.props.key!;
-		const idArr = (id + '').split('').map(s => parseInt(s, 10));
-		this.props.key = '';
-
-		return key.split('').map((char) => {
-			let charCode = char.charCodeAt(0);
-			for (const idChar of idArr) {
-				charCode = charCode ^ idChar;
-			}
-			return charCode;
-		}).join('');
-	}
-
-	private _connection: {
-		id: string;
-		clientSecret: string;
-	}|null = null;
-	protected async assertConnection() {
-		if (this._connection) return true;
-
-		try {
-			const response = await fetch(`${location.origin}/authid`, {
-				method: 'POST'
-			});
-			if (!response.ok) {
-				MessageToast.create({
-					message: `Failed to establish secure connection ${response.status}`,
-					duration: 5000
-				});
-				return false;
-			}
-			const id = await response.text();
-			this._connection = {
-				id,
-				clientSecret: this._createClientSecret(parseInt(id, 10))
-			}
-			return true;
-		} catch(e) {
-			MessageToast.create({
-				message: 'Failed to establish secure connection (network error)',
-				duration: 5000
-			});
-			this._connection = null;
-			return false;
-		}
-	}
-
 	protected async assertOnline() {
 		if (navigator.onLine) return;
 		return new Promise((resolve) => {
@@ -100,7 +55,6 @@ export abstract class ServerComm<ELS extends {
 		[key: string]: any;
 	} = {}, errName: string = 'Failed request') {
 		await this.assertOnline();
-		if (!await this.assertConnection()) return false;
 
 		try {
 			const response = await fetch(url, {
@@ -108,17 +62,13 @@ export abstract class ServerComm<ELS extends {
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify({...{
-					id: this._connection!.id,
-					auth: this._connection!.clientSecret
-				}, ...postBody})
+				body: JSON.stringify({...AUTH, ...postBody})
 			});
 			if (!response.ok) {
 				MessageToast.create({
 					message: `${errName} ${response.status}`,
 					duration: 5000
 				});
-				this._connection = null;
 				return false;
 			}
 			return response;
@@ -127,7 +77,6 @@ export abstract class ServerComm<ELS extends {
 				message: `${errName} (network error)`,
 				duration: 5000
 			});
-			this._connection = null;
 			return false;
 		}
 	}
