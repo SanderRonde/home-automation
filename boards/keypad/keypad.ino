a45242ee5cc7c5a2b528d08ea6650ce1852c95bf
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-08-27 00:08:25 +0200

    finish screen

--- boards/keypad/keypad.ino
+++ boards/keypad/keypad.ino
@@ -1,109 +1,170 @@
-#define Serial1 Serial
-#define Serial2 Serial
-
 #include <Arduino.h>
 #include <string.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <Nextion.h>
+#include <stdio.h>
+#include <SoftwareSerial.h>
 #include "keypad_types.h"
+#include <Nextion.h>
 
-#define SECRET_KEY String("***REMOVED***");
+#define DO_DIM 1
+#define DIM_TIME 60
+#define DIM_BRIGHTNESS 1
+#define NORMAL_BRIGHTNESS 100
 
-NexButton ceiling_on = NexButton(0, 2, "ce_on");
-NexButton ceiling_off = NexButton(0, 6, "ce_off");
-NexButton nightstand_on = NexButton(0, 7, "ns_on");
-NexButton nightstand_off = NexButton(0, 8, "ns_off");
-NexButton lamps_on = NexButton(0, 13, "l_on");
-NexButton lamps_off = NexButton(0, 14, "l_off");
-NexButton couch_on = NexButton(0, 9, "co_on");
-NexButton couch_off = NexButton(0, 10, "co_off");
-NexButton desk_on = NexButton(0, 11, "d_on");
-NexButton desk_off = NexButton(0, 12, "d_off");
-NexButton sound_on = NexButton(0, 15, "s_on");
-NexButton sound_off = NexButton(0, 16, "s_off");
-
-NexTouch *listen_list[] = {
-	&ceiling_on,
-	&ceiling_off,
-	&nightstand_on,
-	&nightstand_off,
-	&lamps_on,
-	&lamps_off,
-	&couch_on,
-	&couch_off,
-	&desk_on,
-	&desk_off,
-	&sound_on,
-	&sound_off,
-	NULL
-};
-
-int send_request(String key, String value) {
-	HTTPClient http;
-	http.setTimeout(2000);
-
-	String path = String("/keyval/");
-	path += key;
-	path += "/";
-	path += value;
-
-	http.begin("***REMOVED***", 80, path);
-	http.addHeader("Content-Type", "application/json");
-
-	String data = String("{\"auth\": \"");
-	data += SECRET_KEY;
-	data += "\"";
-	data += "\"}";
-
-	int httpCode = http.POST(data);
-	if (httpCode > 0) {
-		if (httpCode == 200) {
-			http.end();
-			return 1;
-		} else  {
-			http.end();
-			return 0;
-		}
-	} else {
-		http.end();
-		return 0;
-	}
-}
-
-void onPush(void* ptr) {
-	descriptor_t* descr = (descriptor_t*) ptr;
-	send_request(descr->key, String(descr->value));
-};
-
-descriptor_t* gen_descriptor(String key, STATE_t value) {
-	descriptor_t* descriptor = (descriptor_t*) malloc(sizeof(descriptor_t));
-	descriptor->key = key;
-	descriptor->value = value;
-	return descriptor;
-};
+SoftwareSerial nextion(2, 3);
+Nextion myNextion(nextion, 9600);
 
 void setup() {
 	Serial.begin(9600);
-	delay(500);
+	myNextion.init();
+}
+
+instruction_t ids[] = {
+	{ "", -1 },
+	{ "", -1 },
+	{ "lights.ceiling", 1 },
+	{ "", -1 },
+	{ "", -1 },
+	{ "", -1 },
+	{ "lights.ceiling", 0 },
+	{ "lights.nightstand", 1 },
+	{ "lights.nightstand", 0 },
+	{ "speakers.couch", 1 },
+	{ "speakers.couch", 0 },
+	{ "speakers.desk", 1 },
+	{ "speakers.desk", 0 },
+	{ "lights", 1 },
+	{ "lights", 0 },
+	{ "speakers", 1 },
+	{ "speakers", 0 }
+};
+
+const int SPLIT_LEN = 10;
+char** split_string(String str) {
+	char** strings = (char**) malloc(sizeof(char*) * SPLIT_LEN);
+	for (int i = 0; i < SPLIT_LEN; i++) {
+		strings[i] = NULL;
+	}
+
+	int str_index = 0;
+	char* delim = { " " };
+	char* ptr = strtok(str.c_str(), delim);
+	while (ptr != NULL) {
+		strings[str_index++] = strdup(ptr);
+		ptr = strtok(NULL, delim);
+	}
+
+	return strings;
+}
+
+void free_split(char** strings) {
+	for (int i = 0; i < SPLIT_LEN; i++) {
+		if (strings[i] != NULL) {
+			free(strings[i]);
+		}
+	}
+	free(strings);
+}
+
+msg_t* parse_msg(String message) {
+	char** split = split_string(message);
+	msg_t* msg = (msg_t*) malloc(sizeof(msg_t));
+
+	msg->event_type = strtol(split[0], NULL, 10);
+	msg->screen = strtol(split[1], NULL, 10);
+	msg->id = strtol(split[2], NULL, 16);
+	msg->action = strtol(split[3], NULL, 10);
+	msg->strings = split;
 	
-	ceiling_on.attachPush(onPush, (void*) gen_descriptor("lights.ceiling", ON));
-	ceiling_off.attachPush(onPush, (void*) gen_descriptor("lights.ceiling", OFF));
-	nightstand_on.attachPush(onPush, (void*) gen_descriptor("lights.nightstand", ON));
-	nightstand_off.attachPush(onPush, (void*) gen_descriptor("lights.nightstand", OFF));
-	lamps_on.attachPush(onPush, (void*) gen_descriptor("lights", ON));
-	lamps_off.attachPush(onPush, (void*) gen_descriptor("lights", OFF));
-	couch_on.attachPush(onPush, (void*) gen_descriptor("speakers.couch", ON));
-	couch_off.attachPush(onPush, (void*) gen_descriptor("speakers.couch", OFF));
-	desk_on.attachPush(onPush, (void*) gen_descriptor("speakers.desk", ON));
-	desk_off.attachPush(onPush, (void*) gen_descriptor("speakers.desk", OFF));
-	sound_on.attachPush(onPush, (void*) gen_descriptor("speakers", ON));
-	sound_off.attachPush(onPush, (void*) gen_descriptor("speakers", OFF));
+	return msg;
+}
+
+void free_msg(msg_t* msg) {
+	free_split(msg->strings);
+	free(msg);
+}
+
+void handle_click(msg_t* msg) {
+	if (msg->id == NULL) return;
+
+	instruction_t ins = ids[msg->id];
+	if (ins.value == -1) return;
+
+	Serial.print(ins.name);
+	Serial.print(" ");
+	Serial.println(ins.value);
+}
+
+unsigned long last_touch = 0;
+bool is_dimmed = false;
+bool enable_dimming = true;
+void wake_screen() {
+	if (!is_dimmed) return;
+
+	String dim = "dim=" + String(NORMAL_BRIGHTNESS);
+	myNextion.sendCommand(dim.c_str());
+	is_dimmed = false;
+}
+
+void dim_screen() {
+	if (is_dimmed) return;
+
+	String dim = "dim=" + String(DIM_BRIGHTNESS);
+	myNextion.sendCommand(dim.c_str());
+	is_dimmed = true;
+}
+
+void on_touch() {
+	last_touch = millis();
+	if (is_dimmed) {
+		wake_screen();
+	}
+}
+
+void check_dim() {
+	if (!DO_DIM || !enable_dimming || is_dimmed) return;
+
+	if (millis() - last_touch > DIM_TIME * 1000) {
+		dim_screen();
+	}
+}
+
+void handle_message(String message) {
+	msg_t* msg = parse_msg(message);
+
+	if (msg->event_type == 65) {
+		on_touch();
+		handle_click(msg);
+	}
+
+	free_msg(msg);
+}
+
+void handle_serial(String str) {
+	if (str == "1") {
+		enable_dimming = false;
+		wake_screen();
+	} else if (str == "0") {
+		enable_dimming = true;
+		check_dim();
+	}
+}
+
+void read_serial() {
+	String str = "";
+	while (Serial.available()) {
+		str += String((char) Serial.read());
+	}
+
+	if (str != "") {
+		handle_serial(str);
+	}
 }
 
 void loop() {
-	nexLoop(listen_list);
-	
+	String message = myNextion.listen(); //check for message
+	if (message != "") { // if a message is received...
+		handle_message(message);
+	}
+	read_serial();
+	check_dim();
 }
--- boards/keypad/keypad_types.h
+++ boards/keypad/keypad_types.h
@@ -9,3 +9,16 @@ typedef struct descriptor {
 	String key;
 	STATE_t value;
 } descriptor_t;
+
+typedef struct msg {
+	long event_type;
+	long screen;
+	long id;
+	long action;
+	char** strings;
+} msg_t;
+
+typedef struct instruction {
+	char* name;
+	int value;
+} instruction_t;
