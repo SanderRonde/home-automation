d3351815f94f2fa534111927067e084d23bffe14
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-01-23 00:47:36 +0100

    add time util functions

--- app/server/lib/util.ts
+++ app/server/lib/util.ts
@@ -35,3 +35,46 @@ export async function awaitCondition(
 		tests++;
 	}
 }
+
+export namespace Time {
+	export function toTime(timeStr: string): Time {
+		const [hours, minutes] = timeStr.split(':');
+		return {
+			hours: parseInt(hours, 10),
+			minutes: parseInt(minutes, 10)
+		};
+	}
+
+	export interface Time {
+		hours: number;
+		minutes: number;
+	}
+
+	export function dateToTime(date: Date): Time {
+		const hours = date.getHours();
+		const mins = date.getMinutes();
+		return {
+			hours,
+			minutes: mins
+		};
+	}
+
+	function timeToMinutes({ hours, minutes }: Time): number {
+		return hours * 60 + minutes;
+	}
+
+	export function isInRange(time: Time, from: Time, to: Time): boolean {
+		const timeMins = timeToMinutes(time);
+		const fromMins = timeToMinutes(from);
+		const toMins = timeToMinutes(to);
+
+		if (fromMins > toMins) {
+			if (timeMins > fromMins) return true;
+			if (timeMins < toMins) return true;
+			return false;
+		} else {
+			if (timeMins < fromMins || timeMins > toMins) return false;
+			return true;
+		}
+	}
+}
