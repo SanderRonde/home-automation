14833ad88aaa62c84fe7cc4c1a4fd7469b9a288a
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-01-27 02:56:36 +0100

    implement realtime test (almost done)

--- ai/beat-detector/modes/realtime_test/realtime_test.py
+++ ai/beat-detector/modes/realtime_test/realtime_test.py
@@ -1,56 +1,108 @@
 """Main entrypoint for realtime test mode"""
 
 from lib.log import logline, error, enter_group, exit_group
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from ..model import create_model, apply_weights
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+# from ..model import create_model, apply_weights
 from lib.io import IO, IOInput
-from typing import List
+from functools import partial
+from typing import List, Any
+from http import HTTPStatus
 from glob import glob
-import SocketServer
+import youtube_dl
+import json
+import os
+
+CUR_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
 
 def get_io() -> IO:
     return IO(
         {
             "p": IOInput(
-                1234,
-				int,
-				has_input=True,
-				arg_name="port",
-				descr="The port on which to host it",
-				alias="port"
+                1234, int, has_input=True, arg_name="port", descr="The port on which to host it", alias="port"
             ),
-			# TODO: maybe remove N?
             "n": IOInput(
-                50,
-                int,
-                has_input=True,
-                arg_name="interval",
-                descr="Interval at which data is sent",
-                alias="interval"
-            )
+                50, int, has_input=True, arg_name="interval", descr="Interval at which data is sent", alias="interval"
+            ),
         }
     )
 
-class WebServer(BaseHTTPRequestHandler):
-	def do_GET(self):
-		pass
+interval: int = 100
+model = None
+class WebServer(SimpleHTTPRequestHandler):
+    def get_public_url(self, url: str):
+        return "/files/{}.mp3".format(url)
 
-	def do_post(self):
-		pass
+    def dl_exists(self, url: str):
+        return os.path.isfile(os.path.join(CUR_DIR, "public/files", "{}.mp3".format(url)))
+
+    def download_url(self, url: str):
+        dl = youtube_dl.YoutubeDL({
+            "format": "bestaudio",
+            "outtmpl": os.path.join(CUR_DIR, "public/files/{}.mp3".format(url)),
+            "verbose": True
+        })
+        print("Starting download")
+        dl.download([url])
+
+    def parse_json(self):
+        length = int(self.headers["Content-Length"])
+        return json.loads(self.rfile.read(length))
+
+    def respond_json(self, data: Any, status=HTTPStatus.OK):
+        self.send_response(status)
+        self.send_header("Content-Type", "application/json")
+        content = json.dumps(data).encode("utf8")
+        self.send_header("Content-Length", len(content))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def handle_api(self):
+        if self.path.startswith("/api/beat"):
+            parsed = self.parse_json()
+            # TODO: make prediction
+        elif self.path.startswith("/api/dl"):
+            url = self.parse_json()["url"]
+            if self.dl_exists(url):
+                return self.respond_json({"done": True, "url": self.get_public_url(url)})
+            self.download_url(url)
+            self.respond_json({"status": "downloading"})
+        elif self.path.startswith("/api/dlReady"):
+            url = self.parse_json()["url"]
+            exists = self.dl_exists(url)
+            self.respond_json({"done":exists, "url": self.get_public_url(url)})
+        elif self.path.startswith("/api/interval"):
+            self.respond_json({"interval": interval})
+        else:
+            self.respond_json({"?": "?"}, 404)
+
+    def do_POST(self):
+        path = self.path
+        if path.startswith("/api"):
+            return self.handle_api()
+
+        self.respond_json({"?": "?"}, 404)
 
 
 def start_server(io: IO):
-	port = io.get("port")
-	httpd = WebServer(HTTPServer, ('', port))
-	logline("listening at port", port)
-	enter_group()
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
-	exit_group()
-	logline("stopped listening")
+    global interval
+    interval = io.get("interval")
+
+    port = io.get("port")
+    httpd = HTTPServer(
+        ("", port), partial(WebServer, directory=os.path.join(CUR_DIR, "public"))
+    )
+    logline("listening at port", port)
+    enter_group()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    exit_group()
+    logline("stopped listening")
+
 
 def mode_realtime_test():
     """The main realtime test entrypoint"""
@@ -60,12 +112,10 @@ def mode_realtime_test():
     enter_group()
 
     logline("reconstructing model")
+    global model
     model = create_model(1)
 
     logline("applying learned weights")
     model = apply_weights(model, io.get("input_weights"))
 
-	start_server(io)
-
-	# TODO: start server
-	# TODO: create some API endpoint
+    start_server(io)
