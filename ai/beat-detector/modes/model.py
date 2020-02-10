65a34c841d15c7ac67639ea8309c2a2851315ee2
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-02-10 22:25:40 +0100

    change format

--- ai/beat-detector/modes/model.py
+++ ai/beat-detector/modes/model.py
@@ -11,30 +11,29 @@ RECURRENT_DROPOUT = 0.2
 
 
 def create_model(batch_size: int) -> Sequential:
-    model = Sequential(name="beat_detector")
+    model = Sequential(
+        [
+            LSTM(
+                BINS,
+                input_shape=(Features.length(), 1),
+                batch_size=batch_size,
+                return_sequences=True,
+                stateful=True,
+                dropout=DROPOUT,
+                recurrent_dropout=RECURRENT_DROPOUT,
+            ),
+            LSTM(
+                BINS,
+                batch_size=batch_size,
+                return_sequences=False,
+                stateful=True,
+                dropout=DROPOUT,
+                recurrent_dropout=RECURRENT_DROPOUT,
+            ),
+            Dense(OUT_VEC_SIZE, activation="relu"),
+        ]
+    )
 
-    model.add(
-        LSTM(
-            BINS,
-            input_shape=(Features.length(), 1),
-            batch_size=batch_size,
-            return_sequences=True,
-            stateful=True,
-            dropout=DROPOUT,
-            recurrent_dropout=RECURRENT_DROPOUT,
-        )
-    )
-    model.add(
-        LSTM(
-            BINS,
-            batch_size=batch_size,
-            return_sequences=False,
-            stateful=True,
-            dropout=DROPOUT,
-            recurrent_dropout=RECURRENT_DROPOUT,
-        )
-    )
-    model.add(Dense(OUT_VEC_SIZE, activation="relu"))
     model.compile(loss="mean_squared_error", optimizer="adam")
 
     return model
