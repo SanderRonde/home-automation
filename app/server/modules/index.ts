c6dcf0a43ef7d3bab224748c2f91a4a80dbe1e19
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-05-21 19:09:49 +0200

    make sure exports don't need updating

--- app/server/modules/index.ts
+++ app/server/modules/index.ts
@@ -1,18 +1,1 @@
-export {
-	RemoteControl,
-	HomeDetector,
-	SpotifyBeats,
-	Temperature,
-	Pressure,
-	Movement,
-	Explain,
-	Webhook,
-	Script,
-	KeyVal,
-	Cast,
-	RGB,
-	Bot,
-	Auth
-} from './modules';
-
-export { getAllModules, AllModules } from './modules';
+export * from './modules';
