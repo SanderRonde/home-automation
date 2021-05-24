f17022ba11c0f7c3da8207926a36fc9d6a370ffb
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-05-24 04:02:01 +0200

    fix eslint warnings

--- app/server/config_examples/home-hooks.ts
+++ app/server/config_examples/home-hooks.ts
@@ -3,19 +3,19 @@ import { HomeHooks } from '../modules/home-detector';
 export default {
 	phone: {
 		home: {
-			'lights-on': (modules) => {
-				return modules.keyval.set('room.lights.ceiling', '1');
+			'lights-on': async (modules) => {
+				await modules.keyval.set('room.lights.ceiling', '1');
 			},
-			'pc-on': (modules) => {
-				return modules.script.script('wake_desktop');
+			'pc-on': async (modules) => {
+				await modules.script.script('wake_desktop');
 			},
 		},
 		away: {
-			'lights-off': (modules) => {
-				return modules.keyval.set('room.lights', '0');
+			'lights-off': async (modules) => {
+				await modules.keyval.set('room.lights', '0');
 			},
-			'speakers-off': (modules) => {
-				return modules.keyval.set('room.speakers', '0');
+			'speakers-off': async (modules) => {
+				await modules.keyval.set('room.speakers', '0');
 			},
 		},
 	},
