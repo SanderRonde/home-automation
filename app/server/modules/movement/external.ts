2c3c34608eea08393c92c9a8ef5af8d0290848f7
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-08-05 23:20:20 +0200

    require no init for external class

--- app/server/modules/movement/external.ts
+++ app/server/modules/movement/external.ts
@@ -2,7 +2,7 @@ import { Movement } from '.';
 import { createExternalClass } from '../../lib/external';
 import { APIHandler } from './api';
 
-export class ExternalHandler extends createExternalClass(true) {
+export class ExternalHandler extends createExternalClass(false) {
 	async reportMovement(key: string): Promise<void> {
 		return this.runRequest(async (res, source) => {
 			return APIHandler.reportMovement(
