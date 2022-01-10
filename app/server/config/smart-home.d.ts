ce38b722a89f86aaafc20dd16dfae6ab3873e2f2
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-01-10 01:26:47 +0100

    implement abstract class

--- app/server/config/smart-home.d.ts
+++ app/server/config/smart-home.d.ts
@@ -2,6 +2,12 @@ import {
 	SmartHomeDevice,
 } from '../lib/smart-home/smart-home-classes';
 
-declare const devices: (typeof SmartHomeDevice)[];
+declare class NonAbstractSmartHomeDevice extends SmartHomeDevice {
+	public id: string;
+	public name: string;
+	public nicknames: string[];
+}
+
+declare const devices: (typeof NonAbstractSmartHomeDevice)[];
 
 export default devices;
