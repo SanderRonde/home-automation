e81c7364a45cfc7fea29d9ec884c0fa90c707c89
Sander Ronde <awsdfgvhbjn@gmail.com>
2023-05-18 16:16:31 +0200

    only connect to tuya device when needed

--- app/server/modules/tuya/devices/device.ts
+++ app/server/modules/tuya/devices/device.ts
@@ -10,7 +10,7 @@ export class TuyaDevice {
 	 * arbitrarily chosen. However, the lowest number is always the
 	 * power value. This is the value we care about.
 	 */
-	private _lastLowestDigit: number | null = null;
+	private _lastLowestDigit: number = 20;
 	private _active: boolean | null = null;
 
 	public constructor(
@@ -27,19 +27,17 @@ export class TuyaDevice {
 			issueRefreshOnPing: true,
 		});
 
-		this._device.on('disconnected', () => {
-			setTimeout(() => {
-				void this._device.find().then(() => {
-					void this._device.connect();
-				});
-			}, 1000 * 10);
-		});
-
+		void this._device
+			.find({
+				all: false,
+			})
+			.then(() => {
+				void this._device.connect();
+			});
 		const onChange = async (value: boolean) => {
-			if (this._lastLowestDigit === null) {
-				return;
+			if (!this._device.isConnected()) {
+				await this._device.connect();
 			}
-
 			this._active = value;
 			await this._device.set({
 				set: value,
@@ -63,11 +61,6 @@ export class TuyaDevice {
 				'TUYA.DEVICE.STATUS'
 			).set(this._keyval, isEnabled ? '1' : '0', true);
 
-			if (!this._lastLowestDigit !== null) {
-				this._lastLowestDigit = lowestNumberKey;
-				// Update immediately
-				void onChange(isEnabled);
-			}
 			this._lastLowestDigit = lowestNumberKey;
 		});
 
