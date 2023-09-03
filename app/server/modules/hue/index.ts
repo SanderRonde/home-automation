e0c3665045b64741c3c78be38942b2379b839333
Sander Ronde <awsdfgvhbjn@gmail.com>
2023-09-03 21:45:03 +0200

    perform hue bridge discovery outside of main init loop

--- app/server/modules/hue/index.ts
+++ app/server/modules/hue/index.ts
@@ -1,4 +1,3 @@
-import { Api } from 'node-hue-api/dist/esm/api/Api';
 import { discoverBridge } from './discover-bridge';
 import { linkHueDevices } from '../../config/hue';
 import { logTag } from '../../lib/logger';
@@ -7,10 +6,13 @@ import { ModuleMeta } from '../meta';
 import { AllModules } from '..';
 
 export const Hue = new (class Meta extends ModuleMeta {
-	private _bridge: Api | null = null;
 	public name = 'hue';
 
-	public async init() {
+	public init() {
+		return Promise.resolve();
+	}
+
+	public async notifyModules(modules: unknown) {
 		// If you don't have a username, uncomment this line and run the server once
 		// Be sure to press the connect button before calling this!
 		// await createUser();
@@ -19,17 +21,17 @@ export const Hue = new (class Meta extends ModuleMeta {
 		if (!hueUsername) {
 			return;
 		}
-		try {
-			this._bridge = await discoverBridge(hueUsername);
-		} catch (e) {
+		const bridge = await (async () => {
+			try {
+				return await discoverBridge(hueUsername);
+			} catch (e) {
+				return null;
+			}
+		})();
+		if (!bridge) {
 			logTag('hue', 'red', 'Failed to connect to hue bridge');
-		}
-	}
-
-	public async notifyModules(modules: unknown) {
-		if (!this._bridge) {
 			return;
 		}
-		await linkHueDevices(this._bridge, modules as AllModules);
+		await linkHueDevices(bridge, modules as AllModules);
 	}
 })();
