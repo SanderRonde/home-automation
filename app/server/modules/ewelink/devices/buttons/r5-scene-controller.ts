109cd376009e1c28e5e973be9a01a439c66b05aa
Sander Ronde <awsdfgvhbjn@gmail.com>
2024-09-26 23:35:25 +0200

    flip r5 scene controller buttons

--- app/server/modules/ewelink/devices/buttons/r5-scene-controller.ts
+++ app/server/modules/ewelink/devices/buttons/r5-scene-controller.ts
@@ -4,9 +4,9 @@ export enum SceneControllerTriggerType {
 	TOP_LEFT = 5,
 	TOP_MIDDLE = 4,
 	TOP_RIGHT = 3,
-	BOTTOM_LEFT = 0,
+	BOTTOM_LEFT = 2,
 	BOTTOM_MIDDLE = 1,
-	BOTTOM_RIGHT = 2,
+	BOTTOM_RIGHT = 0,
 }
 
 export class EWeLinkSceneController extends EwelinkKeyvalButtonBase<SceneControllerTriggerType> {
