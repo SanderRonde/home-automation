a04c4f304edb053f6bed2be0a11d2a2a4e48a12f
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-08-09 01:32:18 +0200

    improve event-emitter

--- app/server/lib/event-emitter.ts
+++ app/server/lib/event-emitter.ts
@@ -1,15 +1,69 @@
-export class EventEmitter<V> {
-	public lastValue: V | null = null;
-	public handlers: ((value: V) => void)[] = [];
+import { SettablePromise } from './util';
 
-	public listen(handler: (value: V) => void): void {
-		this.handlers.push(handler);
+export class EventEmitter<V, M = V> {
+	protected _handlers: Set<(value: M) => void> = new Set();
+
+	public constructor() {}
+
+	public listen(handler: (value: M) => void): () => void {
+		this._handlers.add(handler);
+
+		return () => this.removeListener(handler);
+	}
+
+	public removeListener(handler: (value: M) => void): void {
+		this._handlers.delete(handler);
+	}
+
+	protected _emit(value: M): void {
+		for (const handler of this._handlers) {
+			handler(value);
+		}
 	}
 
 	public emit(value: V): void {
-		this.lastValue = value;
-		for (const handler of this.handlers) {
-			handler(value);
+		this._emit(value as unknown as M);
+	}
+}
+
+export class AsyncEventEmitter<V, M = V> extends EventEmitter<V, M> {
+	protected _initialValue: SettablePromise<M> = new SettablePromise();
+	private _initialized = false;
+	private _value: Promise<M> = this._initialValue.value;
+
+	public constructor(private readonly initializer?: () => Promise<V>) {
+		super();
+	}
+
+	public get value(): Promise<M> {
+		if (!this._initialized) {
+			this._initialized = true;
+			if (this.initializer) {
+				void this.initializer().then((value) => this.emit(value));
+			}
 		}
+		return this._value;
+	}
+
+	protected _emit(value: M): void {
+		this._initialValue.set(value);
+		this._value = Promise.resolve(value);
+		super._emit(value);
+	}
+}
+
+export class MappedAsyncEventEmitter<V, M = V> extends AsyncEventEmitter<V, M> {
+	public constructor(
+		private readonly mapper?: (value: V) => M,
+		initializer?: () => Promise<V>
+	) {
+		super(initializer);
+	}
+
+	public emit(value: V): void {
+		const mapped = this.mapper
+			? this.mapper(value)
+			: (value as unknown as M);
+		this._emit(mapped);
 	}
 }
