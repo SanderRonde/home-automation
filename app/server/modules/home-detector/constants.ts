cf4e6bf23aa4a14456cdbf437b1f659bdc9e5612
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-11-05 00:08:29 +0100

    Make away grace period longer

--- app/server/modules/home-detector/constants.ts
+++ app/server/modules/home-detector/constants.ts
@@ -2,4 +2,4 @@ export const AWAY_PING_INTERVAL = 10 * 1000;
 export const HOME_PING_INTERVAL = 60 * 1000;
 export const CHANGE_PING_INTERVAL = 10 * 1000;
 export const AWAY_MIN_CONSECUTIVE_PINGS = 10;
-export const GRACE_PERIOD_PINGS = 20;
+export const GRACE_PERIOD_PINGS = 60 * 5;
