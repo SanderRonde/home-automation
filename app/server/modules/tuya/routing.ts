0e825814ecf3885653f450b9400dcaf42783a99d
Sander Ronde <awsdfgvhbjn@gmail.com>
2025-11-27 22:52:47 +0100

    Formatting

--- app/server/modules/tuya/routing.ts
+++ app/server/modules/tuya/routing.ts
@@ -54,4 +54,3 @@ function _initRouting(db: Database<TuyaDB>) {
 export const initRouting = _initRouting as (db: Database<TuyaDB>) => ServeOptions<unknown>;
 
 export type TuyaRoutes = ReturnType<typeof _initRouting> extends ServeOptions<infer R> ? R : never;
-
