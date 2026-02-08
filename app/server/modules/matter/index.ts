91da220d736da69a21f7ebd4fb162daefd9ec496
Sander Ronde <awsdfgvhbjn@gmail.com>
2026-02-08 23:37:25 +0100

    Re-enable matter

--- app/server/modules/matter/index.ts
+++ app/server/modules/matter/index.ts
@@ -1,5 +1,6 @@
 import { SettablePromise } from '../../lib/settable-promise';
-import type { MatterServer } from './server/server';
+import { DeviceSource } from '../device/device';
+import { MatterServer } from './server/server';
 import type { ModuleConfig } from '../modules';
 import { initRouting } from './routing';
 import { ModuleMeta } from '../meta';
@@ -10,22 +11,22 @@ export const Matter = new (class Matter extends ModuleMeta {
 	public server = new SettablePromise<MatterServer>();
 
 	public init(config: ModuleConfig) {
-		// const matterServer = new MatterServer();
-		// this.server.set(matterServer);
-		// matterServer.start().catch((error) => {
-		// 	console.error('Matter server start error:', error);
-		// });
-		// matterServer.devices.subscribe(async (devices) => {
-		// 	if (!devices) {
-		// 		return;
-		// 	}
-		// 	try {
-		// 		const api = await config.modules.device.api.value;
-		// 		api.setDevices(Object.values(devices), DeviceSource.MATTER);
-		// 	} catch (error) {
-		// 		console.error('Error setting devices:', error);
-		// 	}
-		// });
+		const matterServer = new MatterServer();
+		this.server.set(matterServer);
+		matterServer.start().catch((error) => {
+			console.error('Matter server start error:', error);
+		});
+		matterServer.devices.subscribe(async (devices) => {
+			if (!devices) {
+				return;
+			}
+			try {
+				const api = await config.modules.device.api.value;
+				api.setDevices(Object.values(devices), DeviceSource.MATTER);
+			} catch (error) {
+				console.error('Error setting devices:', error);
+			}
+		});
 
 		return {
 			serve: initRouting(config),
